24c30cc6363aa52d444e58a968020eba68be4b26
zheka007 <59200934+zheka007@users.noreply.github.com>
2019-12-24 18:30:48 +0600

    Delete blank.js

--- blank.js
+++
@@ -1,9 +0,0 @@
-let [name1, name2, ...rest] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
-
-alert(name1); // Julius
-alert(name2); // Caesar
-
-// Обратите внимание, что `rest` является массивом.
-alert(rest[0]); // Consul
-alert(rest[1]); // of the Roman Republic
-alert(rest.length); // 2
